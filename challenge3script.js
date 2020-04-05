edfa29a77a8bcf2c1f1c7efcbc14e3dd09e0511f
ZeynabAbdulhanan <61313171+ZeynabAbdulhanan@users.noreply.github.com>
2020-04-05 13:39:54 +0200

    getapidata

--- challenge3script.js
+++ challenge3script.js
@@ -1,1 +1,32 @@
 
+function getAPIdata() {
+
+	// construct request
+	var request = 'https://api.openweathermap.org/data/2.5/weather?appid=671ed302134d547adf8e79c854664915&q=the%20Hague,nl';
+
+	// get current weather
+	fetch(request)	
+	
+	// parse response to JSON format
+	.then(function(response) {
+		return response.json();
+	})
+	
+	// do something with response
+	.then(function(response) {
+		// show full JSON object
+		//console.log(response);
+		var weatherBox = document.getElementById('weather');
+		//weatherBox.innerHTML = response;
+		//weatherBox.innerHTML = response.weather[0].description;
+		//weatherBox.innerHTML = response.main.temp;
+
+		// var degC = Math.floor(response.main.temp - 273.15);
+		// var weatherBox = document.getElementById('weather');
+		// weatherBox.innerHTML = degC + '&#176;C <br>';
+	});
+}
+
+// init data stream
+getAPIdata();
+
