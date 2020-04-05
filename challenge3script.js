44aae6a3d69dbcfcd15ee125d966d1f57bba855f
ZeynabAbdulhanan <61313171+ZeynabAbdulhanan@users.noreply.github.com>
2020-04-05 14:56:00 +0200

    update codes

--- challenge3script.js
+++ challenge3script.js
@@ -2,7 +2,7 @@
 function getAPIdata() {
 
 	// construct request
-	var request = 'https://api.openweathermap.org/data/2.5/weather?appid=671ed302134d547adf8e79c854664915&q=the%20Hague,nl';
+	var request = 'https://api.openweathermap.org/data/2.5/weather?appid=671ed302134d547adf8e79c854664915&q=Utrecht,nl';
 
 	// get current weather
 	fetch(request)	
@@ -17,13 +17,21 @@ function getAPIdata() {
 		// show full JSON object
 		//console.log(response);
 		var weatherBox = document.getElementById('weather');
-		//weatherBox.innerHTML = response;
+		weatherBox.innerHTML = response;
+        
+        console.log(response.wind.speed);
+        console.log(response.main.humidity);
+        console.log(response.timezone);
+        console.log(response.visibility);
+        
+        //weatherBox.innerHTML = response.wind.speed;
 		//weatherBox.innerHTML = response.weather[0].description;
 		//weatherBox.innerHTML = response.main.temp;
 
-		// var degC = Math.floor(response.main.temp - 273.15);
-		// var weatherBox = document.getElementById('weather');
-		// weatherBox.innerHTML = degC + '&#176;C <br>';
+		var degC = Math.floor(response.main.temp - 273.15);
+		var weatherBox = document.getElementById('weather');
+		weatherBox.innerHTML = degC + '&#176;C <br>';
+        
 	});
 }
 
