c19d73370bb5fe75d3c3f6a52e73e6e2333155e8
ZeynabAbdulhanan <61313171+ZeynabAbdulhanan@users.noreply.github.com>
2020-04-16 00:16:13 +0200

    update

--- main.js
+++ main.js
@@ -167,25 +167,118 @@ function sunRiseSetTimes(jsonData) {
     var text = {
         "data": [
         {
-            "sunrise": "7:27:02 AM",
-            "sunset": "5:05:55 PM",
-            "time": "",
+            "sunrise": "5:05:55 AM </br> <img src='sunrise-100px.svg' class='sunrise' style='width: 40px; padding: 0px'>",
+            "sunset": "5:05:55 PM </br> <img src='sunset-100px.svg' class='sunset' style=' width: 40px; padding: 0px'>",
+            "day_length": "13:10:31",
         
-        },
-        
-        ],
-            "meta": {
-            "cost": 1,
-            "dailyQuota": 50,
-            "lat": 58.7984,
-            "lng": 17.8081,
-            "requestCount": 1,
-        }
+        }]
     }
     
+    document.getElementById('sunrise').innerHTML = text.data[0].sunrise; document.getElementById('sunset').innerHTML = text.data[0].sunset; document.getElementById('daylength').innerHTML = text.data[0].day_length;
     
-        sunRiseSet.innerHTML = text.data[0].sunrise + text.data[0].sunset;
     
+    //sunRiseSet.innerHTML = text.data[0].sunrise +'</br>'+ text.data[0].sunset +'</br>' + text.data[0].day_length;
+}
+
+
+//////////////////////////////////////////////////////map/////////////////////////////////////////////////////////////////////////
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiemV5bmFiYWJkdWxoYW5hbiIsImEiOiJjazkxdjg0ZTcwMWt3M2xtcnk1aDFybHM4In0.7yRO7RB895NjzcHXZNuA7g';
+
+// api token for openWeatherMap
+var openWeatherMapUrl = 'https://api.openweathermap.org/data/2.5/weather';
+var openWeatherMapUrlApiKey = '671ed302134d547adf8e79c854664915';
+
+// Determine cities
+var cities = [
+  {
+    name: 'Amsterdam',
+    coordinates: [4.895168, 52.370216]
+  },
+  {
+    name: 'Paris',
+    coordinates: [48.856613, 2.352222]
+  },
+  {
+    name: 'London',
+    coordinates: [51.507351, -0.127758]
+  },
+  {
+    name: 'New York',
+    coordinates: [40.712776, -74.005974]
+  },
+  {
+    name: 'Jakarta',
+    coordinates: [-6.175110, 106.865036]
+  },
+  {
+    name: 'Dubai',
+    coordinates: [25.204849, 55.270782]
+  },
+];
+
+// Initiate map
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/outdoors-v11',
+  center: [5.508852, 52.142480],
+  zoom: 7
+});
+
+
+// get weather data and plot on map
+map.on('load', function () {
+  cities.forEach(function(city) {
+    // Usually you do not want to call an api multiple times, but in this case we have to
+    // because the openWeatherMap API does not allow multiple lat lon coords in one request.
+    var request = openWeatherMapUrl + '?' + 'appid=' + openWeatherMapUrlApiKey + '&lon=' + city.coordinates[0] + '&lat=' + city.coordinates[1];
+
+    // Get current weather based on cities' coordinates
+    fetch(request)
+      .then(function(response) {
+        if(!response.ok) throw Error(response.statusText);
+        return response.json();
+      })
+      .then(function(response) {
+        // Then plot the weather response + icon on MapBox
+        plotImageOnMap(response.weather[0].icon, city)
+      })
+      .catch(function (error) {
+        console.log('ERROR:', error);
+      });
+  });
+});
+
+function plotImageOnMap(icon, city) {
+  map.loadImage(
+    'http://openweathermap.org/img/w/' + icon + '.png',
+    function (error, image) {
+      if (error) throw error;
+      map.addImage("weatherIcon_" + city.name, image);
+      map.addSource("point_" + city.name, {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: [{
+            type: "Feature",
+            geometry: {
+              type: "Point",
+              coordinates: city.coordinates
+            }
+          }]
+        }
+      });
+      map.addLayer({
+        id: "points_" + city.name,
+        type: "symbol",
+        source: "point_" + city.name,
+        layout: {
+          "icon-image": "weatherIcon_" + city.name,
+          "icon-size": 1.3
+        }
+      });
+    }
+  );
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
